2f8dd0cc3767415d3dc822c538042318c70f4260
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2022-10-14 12:41:04 +0200

    fix typo bug common info

--- pages/common-infos.tsx
+++ pages/common-infos.tsx
@@ -54,116 +54,143 @@ const Home: NextPage = () => {
           will do our best to secure your entrance.
         </p>
         <h2>FESTIVAL VENUES</h2>
-        <ul className="venue-list">
+        <ul className='venue-list'>
           <li>
             <span className='venue-name'>Blå</span>
-            <br/><span className='venue-address'>Brenneriveien 9C</span>
+            <br />
+            <span className='venue-address'>Brenneriveien 9C</span>
           </li>
           <li>
             <span className='venue-name'>Cosmopolite</span>
-            <br/><span className='venue-address'>Vogts gate 64</span>
+            <br />
+            <span className='venue-address'>Vogts gate 64</span>
           </li>
           <li>
             <span className='venue-name'>Dansens Hus</span>
-            <br/><span className='venue-address'>Vulkan 1</span>
+            <br />
+            <span className='venue-address'>Vulkan 1</span>
           </li>
           <li>
             <span className='venue-name'>Den Norske Opera & Ballett</span>
-            <br/><span className='venue-address'>Kirsten Flagstads Plass</span>
+            <br />
+            <span className='venue-address'>Kirsten Flagstads Plass</span>
           </li>
           <li>
             <span className='venue-name'>Vigelandsmuseet</span>
-            <br/><span className='venue-address'>Nobels gate 32</span>
+            <br />
+            <span className='venue-address'>Nobels gate 32</span>
           </li>
           <li>
             <span className='venue-name'>Grønland kulturstasjon</span>
-            <br/><span className='venue-address'>Tøyenbekken 5</span>
+            <br />
+            <span className='venue-address'>Tøyenbekken 5</span>
           </li>
           <li>
             <span className='venue-name'>Jaeger</span>
-            <br/><span className='venue-address'>Grensen 9</span>
+            <br />
+            <span className='venue-address'>Grensen 9</span>
           </li>
           <li>
             <span className='venue-name'>Kulturkirken Jakob</span>
-            <br/><span className='venue-address'>Hausmanns gate 14</span>
+            <br />
+            <span className='venue-address'>Hausmanns gate 14</span>
           </li>
           <li>
             <span className='venue-name'>Munch Museum</span>
-            <br/><span className='venue-address'>Edvard Munchs Plass 1</span>
+            <br />
+            <span className='venue-address'>Edvard Munchs Plass 1</span>
           </li>
           <li>
             <span className='venue-name'>Internasjonalen</span>
-            <br/><span className='venue-address'>Youngstorget 2</span>
+            <br />
+            <span className='venue-address'>Youngstorget 2</span>
           </li>
           <li>
             <span className='venue-name'>Feber (Festivalkontor)</span>
-            <br/><span className='venue-address'>Youngstorget 6</span>
+            <br />
+            <span className='venue-address'>Youngstorget 6</span>
           </li>
           <li>
             <span className='venue-name'>Nobels fredssenter</span>
-            <br/><span className='venue-address'>Brynjulf Buls plass 1</span>
+            <br />
+            <span className='venue-address'>Brynjulf Buls plass 1</span>
           </li>
           <li>
             <span className='venue-name'>Sentralen</span>
-            <br/><span className='venue-address'>Øvre Slottsgate 3</span>
+            <br />
+            <span className='venue-address'>Øvre Slottsgate 3</span>
           </li>
           <li>
             <span className='venue-name'>Becco</span>
-            <br/><span className='venue-address'>Kristian Augusts gate 11</span>
+            <br />
+            <span className='venue-address'>Kristian Augusts gate 11</span>
           </li>
           <li>
             <span className='venue-name'>
               Nordic Black Theatre & Cafeteatret
             </span>
-            <br/><span className='venue-address'>Hollendergata 8</span>
+            <br />
+            <span className='venue-address'>Hollendergata 8</span>
           </li>
           <li>
             <span className='venue-name'>Oslo Konserthus</span>
-            <br/><span className='venue-address'>Munkedamsveien 14</span>
+            <br />
+            <span className='venue-address'>Munkedamsveien 14</span>
           </li>
           <li>
             <span className='venue-name'>Parkteatret</span>
-            <br/><span className='venue-address'>Olaf Ryes plass 11</span>
+            <br />
+            <span className='venue-address'>Olaf Ryes plass 11</span>
           </li>
           <li>
             <span className='venue-name'>Riksscenen</span>
-            <br/><span className='venue-address'>Trondheimsveien 2</span>
+            <br />
+            <span className='venue-address'>Trondheimsveien 2</span>
           </li>
           <li>
             <span className='venue-name'>Rockefeller Music Hall</span>
-            <br/><span className='venue-address'>Badstugata 2</span>
+            <br />
+            <span className='venue-address'>Badstugata 2</span>
           </li>
           <li>
             <span className='venue-name'>Sentrum Scene</span>
-            <br/><span className='venue-address'>Arbeidersamfunnets plass 1</span>
+            <br />
+            <span className='venue-address'>Arbeidersamfunnets plass 1</span>
           </li>
           <li>
             <span className='venue-name'>Victoria Nasjonal Jazzscene</span>
-            <br/><span className='venue-address'>Karl Johans gate 35</span>
+            <br />
+            <span className='venue-address'>Karl Johans gate 35</span>
           </li>
           <li>
             <span className='venue-name'>Hærverk</span>
-            <br/><span className='venue-address'>Hausmanns gate 34</span>
+            <br />
+            <span className='venue-address'>Hausmanns gate 34</span>
           </li>
           <li>
             <span className='venue-name'>Trekanten</span>
-            <br/><span className='venue-address'>St. Olavs Plass 5</span>
+            <br />
+            <span className='venue-address'>St. Olavs Plass 5</span>
           </li>
           <li>
             <span className='venue-name'>Pressens Hus</span>
-            <br/><span className='venue-address'>Skippergata 24</span>
+            <br />
+            <span className='venue-address'>Skippergata 24</span>
           </li>
           <li>
             <span className='venue-name'>Melahuset</span>
-            <br/><span className='venue-address'>Mariboes gate 8</span>
+            <br />
+            <span className='venue-address'>Mariboes gate 8</span>
           </li>
           <li>
             <span className='venue-name'>Asylet</span>
-            <br/><span className='venue-address'>Grønland 28</span>
+            <br />
+            <span className='venue-address'>Grønland 28</span>
           </li>
           <li>
             <span className='venue-name'>Hammerhai</span>
-            <br/>><span className='venue-address'>Opergata 77</span>
+            <br />
+            <span className='venue-address'>Opergata 77</span>
           </li>
         </ul>
       </main>
