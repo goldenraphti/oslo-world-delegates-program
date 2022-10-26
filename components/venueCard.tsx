3e7ae14cf1a40083c34f091adc07d58cb1adfead
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2022-10-26 20:38:16 +0200

    style venue cards for icons

--- components/venueCard.tsx
+++ components/venueCard.tsx
@@ -10,33 +10,37 @@ type Props = {
 export const VenueCard = ({ venue }: Props) => {
   return (
     <li className={styles.venue}>
-      <span className={styles.venueName}>{venue.name}</span>
-      <br />
-      <span className={styles.venueAddress}>{venue.address}</span>
-      <div className={styles.linksContainer}>
-        {venue.googleMapsURL && (
-          <a
-            href={venue.googleMapsURL}
-            target='_blank'
-            rel='noreferrer'
-            className={styles.linkIcon}
-          >
-            <span className='screen-reader'>{`Open ${venue.name} google Maps location in a new tab`}</span>
-            <MapPin size={20} className={styles.iconInLink} />
-          </a>
-        )}
-        {venue.website && (
-          <a
-            href={venue.website}
-            target='_blank'
-            rel='noreferrer'
-            className={styles.linkIcon}
-          >
-            <span className='screen-reader'>{`Open ${venue.name} website in a new tab`}</span>
-            <Home size={20} className={styles.iconInLink} />
-          </a>
-        )}
-      </div>
+      <p className={styles.textContent}>
+        <span className={styles.venueName}>{venue.name}</span>
+        <br />
+        <span className={styles.venueAddress}>{venue.address}</span>
+      </p>
+      {(venue.googleMapsURL || venue.website) && (
+        <div className={styles.linksContainer}>
+          {venue.googleMapsURL && (
+            <a
+              href={venue.googleMapsURL}
+              target='_blank'
+              rel='noreferrer'
+              className={styles.linkIcon}
+            >
+              <span className='screen-reader'>{`Open ${venue.name} google Maps location in a new tab`}</span>
+              <MapPin size={20} className={styles.iconInLink} />
+            </a>
+          )}
+          {venue.website && (
+            <a
+              href={venue.website}
+              target='_blank'
+              rel='noreferrer'
+              className={styles.linkIcon}
+            >
+              <span className='screen-reader'>{`Open ${venue.name} website in a new tab`}</span>
+              <Home size={20} className={styles.iconInLink} />
+            </a>
+          )}
+        </div>
+      )}
     </li>
   );
 };
